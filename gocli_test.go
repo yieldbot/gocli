e6de28607e8f0035338f8152df1fba1332e8f9c8
Fatih Cetinkaya <fcetinkaya@yieldbot.com>
2016-05-31 00:18:15 -0400

    Improve tests

--- gocli_test.go
+++ gocli_test.go
@@ -29,7 +29,10 @@ func init() {
 	flag.BoolVar(&versionFlag, "v", false, "Display version information")
 }
 
-func TestInit(t *testing.T) {
+func TestInit_1(t *testing.T) {
+
+	// Reset the args
+	os.Args = os.Args[:2]
 
 	// Init cli
 	var cli = gocli.Cli{
@@ -69,11 +72,16 @@ func TestInit(t *testing.T) {
 	if len(cli.SubCommandArgs) > 0 {
 		t.Error("invalid SubCommandArgs")
 	}
+}
 
+func TestInit_2(t *testing.T) {
+
+	// Reset the args
+	os.Args = os.Args[:2]
 	os.Args = append(os.Args, "cmd", "arg1")
 
 	// Init cli
-	var cli2 = gocli.Cli{
+	var cli = gocli.Cli{
 		Name:        "test",
 		Version:     "1.0.0",
 		Description: "test desc",
@@ -81,18 +89,56 @@ func TestInit(t *testing.T) {
 			"cmd": "Test command",
 		},
 	}
-	cli2.Init()
+	cli.Init()
 
-	if cli2.SubCommand != "cmd" {
+	if cli.SubCommand != "cmd" {
 		t.Error("invalid SubCommand")
 	}
 
-	if len(cli2.SubCommandArgs) != 1 {
+	if len(cli.SubCommandArgs) != 1 {
 		t.Error("invalid SubCommandArgs")
 	}
 
-	if cli2.SubCommandArgs[0] != "arg1" {
+	if cli.SubCommandArgs[0] != "arg1" {
 		t.Error("invalid SubCommandArgs arg")
+	}
+}
+
+func TestInit_3(t *testing.T) {
+
+	// Reset the args
+	os.Args = os.Args[:2]
+	os.Args = append(os.Args, "cmd", "--arg2", "arg3", "arg4")
+
+	// Init cli
+	var cli = gocli.Cli{
+		Name:        "test",
+		Version:     "1.0.0",
+		Description: "test desc",
+		Commands: map[string]string{
+			"cmd": "Test command",
+		},
+	}
+	cli.Init()
+
+	if cli.SubCommand != "cmd" {
+		t.Error("invalid SubCommand")
+	}
+
+	if len(cli.SubCommandArgs) != 3 {
+		t.Error("invalid SubCommandArgs", cli.SubCommandArgs)
+	}
+
+	if cli.SubCommandArgs[0] != "--arg2" {
+		t.Error("invalid SubCommandArgs arg", cli.SubCommandArgs)
+	}
+
+	if cli.SubCommandArgsMap["arg2"] != "arg3" {
+		t.Error("invalid SubCommandArgsMap arg", cli.SubCommandArgsMap)
+	}
+
+	if _, ok := cli.SubCommandArgsMap["arg4"]; !ok {
+		t.Error("invalid SubCommandArgsMap arg", cli.SubCommandArgsMap)
 	}
 }
 
